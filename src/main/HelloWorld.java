1ed6a9bc0e8e73c7ea715839e8d189495df4e749
HandH1998 <1335248067@qq.com>
2020-09-23 23:49:12 +0800

    第一次学习

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,15 @@
+package main;
+//单行注释
+/*
+    多行注释
+    多行注释
+    多行注释
+ */
+//HelloWorld这个类名必须与文件名完全一致，包括大小写
+public class HelloWorld {
+    //main方法，代表程序执行的起点
+    public static void main(String[] args){
+        //打印输出语句
+        System.out.println("Hello, World!");
+    }
+}
