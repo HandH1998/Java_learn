1886a293b6ba7dc28f5a13273b4f98d85d476b7a
HandH1998 <1335248067@qq.com>
2020-10-14 21:13:09 +0800

    第十七次学习

---
+++ src/multithread/Demo01MainThread.java
@@ -0,0 +1,12 @@
+package multithread;
+
+public class Demo01MainThread {
+    public static void main(String[] args) {
+        Person p1=new Person("小强");
+        p1.run();
+
+        System.out.println(0/0);//ArithmeticException : / by zero
+        Person p2=new Person("旺财");
+        p2.run();
+    }
+}
