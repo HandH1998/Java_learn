eef7825181819ec7145e3ae57025de9a443477ea
HandH1998 <1335248067@qq.com>
2020-10-26 23:05:44 +0800

    第二十四次学习

--- src/io/bufferedstream/Demo05BufferedReader.java
+++ src/io/bufferedstream/Demo05BufferedReader.java
@@ -1,10 +1,11 @@
 package io.bufferedstream;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Demo05BufferedReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         BufferedReader br=new BufferedReader(new FileReader("D:\\IDEA\\Projects\\Java_learn\\src\\io\\bufferedstream\\c.txt"));
         
     }
